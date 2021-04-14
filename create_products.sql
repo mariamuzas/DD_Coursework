99ff5ddb7b13a146cc15a6242ec928c80e5c796f
Your Name <mmuzasantuna@gmail.com>
2021-04-14 12:45:53 +0100

    3 scripts from report done, create_products scrit changed

--- create_products.sql
+++ create_products.sql
@@ -4,7 +4,7 @@ CREATE TABLE Products (
 `product_code` INT(11) NOT NULL,
 `supplier_id` INT(11) NOT NULL,
 `product_name` text NOT NULL,
-`product_type` float NOT NULL,
+`product_price` float NOT NULL,
 PRIMARY KEY(`product_code`),
 FOREIGN KEY (`supplier_id`) REFERENCES Suppliers(supplier_id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- report.sql
+++ report.sql
@@ -1,2 +1,27 @@
 select "Inside script <report.sql>" as ``;
 
+-- List all of the data in the database
+SHOW TABLES;
+
+-- List the first and second names of all customers, in reverse order of second name
+SELECT customer_forename, customer_surname 
+FROM Customers 
+ORDER BY customer_surname DESC;
+
+-- List all customers who have ordered something
+
+SELECT customer_forename, customer_surname 
+FROM Customers 
+WHERE customer_id
+IN ( SELECT customer_id
+    FROM Customer_Orders );
+
+SELECT customer_forename, customer_surname, order_no 
+FROM Customers 
+INNER JOIN Customer_Orders 
+ON customers.customer_id = Customer_Orders.customer_id;
+
+-- Create a VIEW called SUPPLIERS_AND_PRODUCTS that contains the company names from SUPPLIERS and product names and prices from PRODUCTS
+CREATE VIEW Suppliers_and_Products AS
+    SELECT suppliers.company_name, products.product_name, products.product_name
+    FROM Suppliers 
