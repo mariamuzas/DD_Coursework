e3a4f8102865dcf7d8d11ed4874600e79291efb4
Your Name <mmuzasantuna@gmail.com>
2021-04-14 11:54:20 +0100

    load_customers_orders_products script done

--- create_customer_orders_products.sql
+++ create_customer_orders_products.sql
@@ -4,6 +4,6 @@ CREATE TABLE Customer_Orders_Products (
 `order_id` INT(11) NOT NULL,
 `product_id` INT(11) NOT NULL,
 `quantity` INT(11) NOT NULL,
-FOREIGN KEY(order_id) REFERENCES Customer_Orders(order_no) ON DELETE CASCADE,
+FOREIGN KEY(`order_id`) REFERENCES Customer_Orders(order_no) ON DELETE CASCADE,
 FOREIGN KEY (`product_id`) REFERENCES Products(product_code) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- load_customer_orders_products_data.sql
+++ load_customer_orders_products_data.sql
@@ -1,1 +1,5 @@
 select "Inside script <load_customer_orders_products.sql>" as ``;
+LOAD DATA LOCAL INFILE 'customers-orders-products-data.csv'
+  INTO TABLE Customer_Orders_Products
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
