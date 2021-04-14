7fe3e51f248e40ffefdddf45de4f6f0b430818d6
Your Name <mmuzasantuna@gmail.com>
2021-04-14 11:48:43 +0100

    load_customers_orders script done

--- load_customer_orders_data.sql
+++ load_customer_orders_data.sql
@@ -1,1 +1,5 @@
 select "Inside script <load_customer_orders.sql>" as ``;
+LOAD DATA LOCAL INFILE 'customers-orders-data.csv'
+  INTO TABLE Customer_Orders
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
