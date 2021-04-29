3cb6502ffb7b8316ca13ec3a00cfb0747aaa8b8e
Your Name <mmuzasantuna@gmail.com>
2021-04-29 11:45:29 +0100

    report updated, and finished

--- report.sql
+++ report.sql
@@ -1,14 +1,20 @@
 select "Inside script <report.sql>" as ``;
 
 -- List all of the data in the database
+select 'Here is the list of the data that you can find in the database' AS '';
 SHOW TABLES;
 
+
 -- List the first and second names of all customers, in reverse order of second name
+select 'List of the first and second names of all customers, in reverse order of second name' AS '';
+
 SELECT customer_forename, customer_surname 
 FROM Customers 
 ORDER BY customer_surname DESC;
 
+
 -- List all customers who have ordered something
+select 'List of the customers that have ordered something' AS '';
 
 SELECT customers.customer_id, customer_forename, customer_surname 
 FROM Customers 
@@ -23,6 +29,7 @@ FROM Customers
 INNER JOIN Customer_Orders 
 ON customers.customer_id = Customer_Orders.customer_id;
 
+
 -- Create a VIEW called SUPPLIERS_AND_PRODUCTS that contains the company names from SUPPLIERS and product names and prices from PRODUCTS
 DROP VIEW IF EXISTS Suppliers_and_Products;
 CREATE VIEW Suppliers_and_Products AS
@@ -30,4 +37,8 @@ CREATE VIEW Suppliers_and_Products AS
     FROM Suppliers, Products
     WHERE suppliers.supplier_id = products.supplier_id;
 
+
+select 'View created for Suppliers_andProducts' AS '';
 SHOW FULL TABLES;
+
+SELECT * FROM Suppliers_and_Products;
